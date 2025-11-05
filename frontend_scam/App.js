834723f81c367d2c0492fe7bc7a1ebc22a8c09cb
DuskWarden <pawar96sameer@gmail.com>
2025-11-05 13:24:45 +0530

    Update App.js with navigation config and hide header

--- frontend_scam/App.js
+++ frontend_scam/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+// Import all screens
 import HomeScreen from "./src/screens/HomeScreen";
 import MapScreen from "./src/screens/MapScreen";
 import ReportScreen from "./src/screens/ReportScreen";
@@ -9,15 +10,41 @@ import ProfileScreen from "./src/screens/ProfileScreen";
 
 const Stack = createStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Map" component={MapScreen} />
-        <Stack.Screen name="Report" component={ReportScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Navigator 
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false, // hide the top header and back button
+        }}
+      >
+        {/* Home screen with scam list */}
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+        />
+        
+        {/* Map screen for location-based scams */}
+        <Stack.Screen 
+          name="Map" 
+          component={MapScreen}
+        />
+        
+        {/* Report screen to submit new scams */}
+        <Stack.Screen 
+          name="Report" 
+          component={ReportScreen}
+        />
+        
+        {/* Profile screen for user info */}
+        <Stack.Screen 
+          name="Profile" 
+          component={ProfileScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
